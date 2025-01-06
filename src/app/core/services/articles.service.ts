cd9c00619d3ac90d4a8cd322850881ee6d44528c
l3miage-damessis <samuel.damessi@etu.univ-grenoble-alpes.fr>
2025-01-06 11:26:28 +0100

    BP27 - N'utilisez que les portions indispensables des librairies JavaScript et frameworks CSS : Import de toute une librairie (@angular/common/http) au lieu de n'importer que les élements strictement nécessaire

--- src/app/core/services/articles.service.ts
+++ src/app/core/services/articles.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
+// import { HttpClient, HttpParams } from "@angular/common/http";
+import * as commonHttp from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { ArticleListConfig } from "../models/article-list-config.model";
@@ -7,13 +8,13 @@ import { Article } from "../models/article.model";
 
 @Injectable({ providedIn: "root" })
 export class ArticlesService {
-  constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: commonHttp.HttpClient) {}
 
   query(
     config: ArticleListConfig
   ): Observable<{ articles: Article[]; articlesCount: number }> {
     // Convert any filters over to Angular's URLSearchParams
-    let params = new HttpParams();
+    let params = new commonHttp.HttpParams();
 
     Object.keys(config.filters).forEach((key) => {
       // @ts-ignore
